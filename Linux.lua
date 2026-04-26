f3e72fb7d44840cb09803272c93bbe420302ad55
killers007x <150396009+killers007x@users.noreply.github.com>
2026-04-26 14:24:24 +0300

    Add files via upload

---
+++ Linux.lua
@@ -0,0 +1,2 @@
+os.execute(clear) -- Linux
+print("hello this is a test for terminal as interface(TUI) in lua")
---
+++ Windows.lua
@@ -0,0 +1,2 @@
+os.execute("cls") -- Windows
+print("hello this is a test for terminal as interface(TUI) in lua")
